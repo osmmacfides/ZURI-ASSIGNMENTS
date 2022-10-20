917caa805452801d5dbb4cc3f01a3398963eeb28
osmmacfides <okonkwosmmac@gmail.com>
2022-10-20 08:59:26 +0300

    Updated

--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
- return HttpResponse('<h1> THIS IS A MUSICAPP IN A SONGCRUD PROJECT. </h1>')
+ return HttpResponse('<h1> THIS IS A MUSICAPP IN A SONGCRUD PROJECT. <br>My First Python Code</h1>')
