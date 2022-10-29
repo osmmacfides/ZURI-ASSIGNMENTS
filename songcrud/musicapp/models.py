b0dcd22351ab0e188ebb06c08895c28b77767f6f
Okonkwo-Udoye Chiamaka <okonkwosmmac@gmail.com>
2022-10-29 05:32:48 +0300

    Updated

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+  first_name = models.CharField('Artiste first name', max_length=255)
+  last_name = models.CharField( 'Artiste last name', max_length=255)
+  age = models.IntegerField('Artistes Age')
+
+class Song(models.Model):
+  
+  title = models.CharField(max_length=255)
+  date_released = models.CharField( max_length=255)
+  likes = models.CharField(max_length =255)
+  artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+  
+  content = models.CharField(max_length=255)
+  song_id =models.ForeignKey(Song, on_delete=models.CASCADE)
+  
+
+  
+
+  
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,6 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Artiste, Song, Lyric
 
 # Create your views here.
 def home(request):
- return HttpResponse('<h1> THIS IS A MUSICAPP IN A SONGCRUD PROJECT. <br>My First Python Code</h1>')
+    artistes = Artiste.objects.all()
+    songs = Song.objects.all()
+    lyrics = Lyric.objects.all()
+
+    return render(request, "index.html", {
+        "artistes": artistes,  "songs": songs, "lyrics": lyrics
+    })
